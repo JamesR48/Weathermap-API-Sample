06bfaf85dd6dd03c9e2919086fd37fac3ce6a51b
JamesR48 <janu4446@hotmail.com>
2019-12-12 23:27:49 -0500

    Adición de .html para inicio del front end

--- weathertest/openweather/urls.py
+++ weathertest/openweather/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 #Para poder acceder a las vistas creadas es necesario mapearlas a una URL, cosa que hace este archivo URLconf
-
+#Aunque este urls.py es similar al otro urls del proyecto, este contiene los URL relevantes para el app como tal
 # Cada función path() asocia un patrón URL a una vista específica, que se presenta cuando el patrón se empareja
 
 urlpatterns = [
--- weathertest/openweather/views.py
+++ weathertest/openweather/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 #Aquí se crean las vistas
 #Son un tipo de páginas en la app Django que tienen un template y una función específica.
 
+#Para que el template se renderice, se retorna request, que es necesaria para la función render
+#y el nombre del archivo template que se quiere mostrar, en este caso el index.html
 def index(request):
-	return HttpResponse("Test message on the openweather app index.") #la vista index mostraría el mensaje al abrir la aplicación
+	return render(request,'openweather/index.html') #retorna el template en el index.html
