ebc4e4021c4acecce413352dd2aca70be723f1ba
JamesR48 <janu4446@hotmail.com>
2019-12-13 09:09:29 -0500

    Se agrega funcionalidad al formulario para buscar ciudades con el text input y una validación básica

--- weathertest/openweather/views.py
+++ weathertest/openweather/views.py
@@ -13,7 +13,13 @@ def index(request):
 	nextDays_url = 'http://api.openweathermap.org/data/2.5/forecast?q={}&units=metric&appid=5fec5a0ae12df556297c8080de1a434a' #Para utilizar el 5 days forecast API
 	#no se usa el API de 16 days forecast por lo que solo funciona para suscripciones pagas. La desventaja del 5 days forecast es que da el clima cada 3horas
 	#entonces no coincidirían siempre el current con la información del 5 days
-	city = "valledupar"
+	#city = 'valledupar'
+	#request.GET es un diccionario que contiene todos los parámetros de solicitud GET(específicos de Django)
+	#El método get() devuelve un value para la key dada si la esta está en el diccionario. Si no, tiene default 'valledupar'.
+	#request.GET.get() es una combinación de estas que utiliza keys / values de un GET request a la que se puede acceder
+	city=request.GET.get('city','valledupar')
+	if not city:
+		city = 'valledupar' #en caso de que el usuario toque el botón de buscar y no escriba ninguna ciudad manda default 'valledupar'
 	weather_info = requests.get(currentWeather_url.format(city)).json() #Se envía el request a la URL con una ciudad y recibe los datos en JSON de esa ciudad
 	# se pasan a JSON por lo que este tipo de objetos son facilmente representados como diccionarios y listas en Python
 	#Se crea un diccionario para pasar los datos que se requieran al template para que el usuario los vea
